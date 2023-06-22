7881bcc388b0a9bd66d96d75b88f461c93be6fdc
Somiya Ratna Behera <beherasomiyaratna@gmail.com>
2023-06-22 21:21:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -32,10 +32,16 @@ const lookup = {
 };
 
 function rot13(encodedStr) {
-  let decodedArr = []; // Your Result goes here
-  // Only change code below this line
+  let decodedStr = "";
+  for (let i = 0; i < encodedStr.length; i++) {
+    if (lookup[encodedStr[i]] === undefined) {
+      decodedStr += encodedStr[i];
+    } else {
+      decodedStr += lookup[encodedStr[i]];
+    }
+  }
 
-  return; //return decodedArr
+  return decodedStr; //return decodedArr
 }
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
